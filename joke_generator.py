a97e9f21fc0016f1e101280720c5504035bf1c92
AK <allenbkim@gmail.com>
2018-10-21 08:41:53 -0700

    Updated code and added initial models/pickle files for others to try with

--- joke_generator.py
+++ joke_generator.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 
-from keras.models import Model
+from keras.models import Model, load_model
 from keras.layers import Input, LSTM, Dense
 import numpy as np
 import pandas as pd
@@ -8,23 +8,29 @@ import pickle
 
 
 # Constants used in execution
-TRAIN_MODEL = True  # True to run training. False to do just forward pass.
+TRAIN_MODEL = False  # True to run training. False to do just forward pass.
 BATCH_SIZE = 64  # Batch size for training.
-EPOCHS = 1  # Number of epochs to train for.
+EPOCHS = 5  # Number of epochs to train for.
 LATENT_DIM = 256  # Latent dimensionality of the encoding space.
 DATA_PATH = 'jokes.csv'  # Path to the data txt file on disk.
-TRAINING_MODEL_FILE = 'gridsai-qahumor-main.h5'   # Name of the main model file.
-ENCODER_MODEL_FILE = 'gridsai-qahumor-encoder.h5'
-DECODER_MODEL_FILE = 'gridsai-qahumor-decoder.h5'
-PICKLE_FILE = 'gridsai-qahumor-pickle.pckl'
+TRAINING_MODEL_FILE = 'gridsai-qahumor-main-model.h5'   # Name of the main model file.
+ENCODER_MODEL_FILE = 'gridsai-qahumor-encoder-model.h5'
+DECODER_MODEL_FILE = 'gridsai-qahumor-decoder-model.h5'
+ENCODER_PICKLE_FILE = 'gridsai-qahumor-encoder-pickle.pckl'
+DECODER_PICKLE_FILE = 'gridsai-qahumor-decoder-pickle.pckl'
+
+CHAR_TOKENS = ['\n', '\t', ' ', '.', ',', '!', '?', ':', ';', '$', '#', '@', '%', '^', '&', '*', '(', ')', '-', '_',
+               '=', '+', '\\', '/', '|', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+               'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+               'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5',
+               '6', '7', '8', '9', '0', '\"', '\'', '[', ']', '{', '}', '<', '>', '`', '~', '’', 'ø', 'è', '£', 'é',
+               '∫', '—', '͡', '°', '͜', 'ʖ', '']
 
 
 def train_model():
     # Vector-ize the data.
     input_texts = []
     target_texts = []
-    input_characters = set()
-    target_characters = set()
 
     jokes = pd.read_csv(DATA_PATH, header=0)
     for _, row in enumerate(jokes.values):
@@ -34,68 +40,59 @@ def train_model():
         target_text = '\t' + target_text + '\n'
         input_texts.append(input_text)
         target_texts.append(target_text)
-        for char in input_text:
-            if char not in input_characters:
-                input_characters.add(char)
-        for char in target_text:
-            if char not in target_characters:
-                target_characters.add(char)
 
-    input_characters = sorted(list(input_characters))
-    target_characters = sorted(list(target_characters))
-    num_encoder_tokens = len(input_characters)
-    num_decoder_tokens = len(target_characters)
+    characters = sorted(list(CHAR_TOKENS))
+    num_tokens = len(characters)
     max_encoder_seq_length = max([len(txt) for txt in input_texts])
     max_decoder_seq_length = max([len(txt) for txt in target_texts])
 
     print('Number of samples:', len(input_texts))
-    print('Number of unique input tokens:', num_encoder_tokens)
-    print('Number of unique output tokens:', num_decoder_tokens)
+    print('Number of unique tokens:', num_tokens)
     print('Max sequence length for inputs:', max_encoder_seq_length)
     print('Max sequence length for outputs:', max_decoder_seq_length)
 
-    input_token_index = dict(
-        [(char, i) for i, char in enumerate(input_characters)])
-    target_token_index = dict(
-        [(char, i) for i, char in enumerate(target_characters)])
+    token_index = dict(
+        [(char, i) for i, char in enumerate(characters)])
 
     encoder_input_data = np.zeros(
-        (len(input_texts), max_encoder_seq_length, num_encoder_tokens),
+        (len(input_texts), max_encoder_seq_length, num_tokens),
         dtype='float32')
     decoder_input_data = np.zeros(
-        (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
+        (len(input_texts), max_decoder_seq_length, num_tokens),
         dtype='float32')
     decoder_target_data = np.zeros(
-        (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
+        (len(input_texts), max_decoder_seq_length, num_tokens),
         dtype='float32')
 
     for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
         for t, char in enumerate(input_text):
-            encoder_input_data[i, t, input_token_index[char]] = 1.
+            if char in token_index:
+                encoder_input_data[i, t, token_index[char]] = 1.
         for t, char in enumerate(target_text):
             # decoder_target_data is ahead of decoder_input_data by one timestep
-            decoder_input_data[i, t, target_token_index[char]] = 1.
-            if t > 0:
-                # decoder_target_data will be ahead by one timestep
-                # and will not include the start character.
-                decoder_target_data[i, t - 1, target_token_index[char]] = 1.
+            if char in token_index:
+                decoder_input_data[i, t, token_index[char]] = 1.
+                if t > 0:
+                    # decoder_target_data will be ahead by one timestep
+                    # and will not include the start character.
+                    decoder_target_data[i, t - 1, token_index[char]] = 1.
 
     # Define an input sequence and process it.
-    encoder_inputs = Input(shape=(None, num_encoder_tokens))
+    encoder_inputs = Input(shape=(None, num_tokens))
     encoder = LSTM(LATENT_DIM, return_state=True)
     encoder_outputs, state_h, state_c = encoder(encoder_inputs)
     # We discard 'encoder_outputs' and only keep the states.
     encoder_states = [state_h, state_c]
 
-    # Set up the decoder, using `encoder_states` as initial state.
-    decoder_inputs = Input(shape=(None, num_decoder_tokens))
+    # Set up the decoder, using 'encoder_states' as initial state.
+    decoder_inputs = Input(shape=(None, num_tokens))
     # We set up our decoder to return full output sequences,
     # and to return internal states as well. We don't use the
     # return states in the training model, but we will use them in inference.
     decoder_lstm = LSTM(LATENT_DIM, return_sequences=True, return_state=True)
     decoder_outputs, _, _ = decoder_lstm(decoder_inputs,
                                          initial_state=encoder_states)
-    decoder_dense = Dense(num_decoder_tokens, activation='softmax')
+    decoder_dense = Dense(num_tokens, activation='softmax')
     decoder_outputs = decoder_dense(decoder_outputs)
 
     # Define the model that will turn
@@ -135,25 +132,19 @@ def train_model():
         [decoder_outputs] + decoder_states)
     decoder_model.save(DECODER_MODEL_FILE)
 
-    # Reverse-lookup token index to decode sequences back to
-    # something readable.
-    reverse_input_char_index = dict(
-        (i, char) for char, i in input_token_index.items())
-    reverse_target_char_index = dict(
-        (i, char) for char, i in target_token_index.items())
-
     # Pickle some data for use when decoding
-    pickle_file = open(PICKLE_FILE, 'wb')
-    pickle.dump([num_decoder_tokens, target_token_index, reverse_target_char_index, max_decoder_seq_length], pickle_file)
-    pickle_file.close()
+    encoder_pickle_file = open(ENCODER_PICKLE_FILE, 'wb')
+    pickle.dump(max_encoder_seq_length, encoder_pickle_file)
+    encoder_pickle_file.close()
+    decoder_pickle_file = open(DECODER_PICKLE_FILE, 'wb')
+    pickle.dump(max_decoder_seq_length, decoder_pickle_file)
+    decoder_pickle_file.close()
 
 
-def decode_sequence(input_seq):
+def decode_sequence(input_seq, num_decoder_tokens, target_token_index, reverse_target_char_index, max_decoder_seq_length):
     # Load up models and data
-    encoder_model = Model.load_model(ENCODER_MODEL_FILE)
-    decoder_model = Model.load_model(DECODER_MODEL_FILE)
-    pickle_file = open(PICKLE_FILE, 'rb')
-    num_decoder_tokens, target_token_index, reverse_target_char_index, max_decoder_seq_length = pickle.load(pickle_file)
+    encoder_model = load_model(ENCODER_MODEL_FILE)
+    decoder_model = load_model(DECODER_MODEL_FILE)
 
     # Encode the input as state vectors.
     states_value = encoder_model.predict(input_seq)
@@ -200,10 +191,31 @@ def forward_pass(jokes_needing_punchlines):
     """
     decoded_sentences = []
 
+    encoder_pickle_file = open(ENCODER_PICKLE_FILE, 'rb')
+    max_encoder_seq_length = pickle.load(encoder_pickle_file)
+    encoder_pickle_file.close()
+    decoder_pickle_file = open(ENCODER_PICKLE_FILE, 'rb')
+    max_decoder_seq_length = pickle.load(decoder_pickle_file)
+    decoder_pickle_file.close()
+
+    # Reverse-lookup token index to decode sequences back to something readable.
+    characters = sorted(list(CHAR_TOKENS))
+    num_tokens = len(characters)
+    token_index = dict([(char, i) for i, char in enumerate(characters)])
+    reverse_char_index = dict((i, char) for char, i in token_index.items())
+
+    encoder_input_data = np.zeros(
+        (len(jokes_needing_punchlines), max_encoder_seq_length, num_tokens),
+        dtype='float32')
+
+    for i, input_text in enumerate(jokes_needing_punchlines):
+        for t, char in enumerate(input_text):
+            encoder_input_data[i, t, token_index[char]] = 1.
+
     for seq_index in range(len(jokes_needing_punchlines)):
         # Take one sequence for trying out decoding.
-        input_seq = jokes_needing_punchlines[seq_index: seq_index + 1]
-        decoded_sentence = decode_sequence(input_seq)
+        input_seq = encoder_input_data[seq_index: seq_index + 1]
+        decoded_sentence = decode_sequence(input_seq, num_tokens, token_index, reverse_char_index, max_decoder_seq_length)
         decoded_sentences.append((jokes_needing_punchlines[seq_index], decoded_sentence))
 
     return decoded_sentences
@@ -217,12 +229,12 @@ if TRAIN_MODEL:
     train_model()
 
 # After model is trained, run forward pass with new joke setups that need punchlines
-punchlines = forward_pass(['Why did the chicken cross the road?',
-                           'Who let the dogs out?',
-                           'How much wood would a woodchuck chuck?',
-                           'What did the Trojan say to the Bruin?',
-                           'When is the best time to wear a hat?',
-                           'Where is the best Data Science city?'])
+punchlines = forward_pass(['\tWhy did the chicken cross the road?\n',
+                           '\tWho let the dogs out?\n',
+                           '\tHow much wood would a woodchuck chuck?\n',
+                           '\tWhat did the Trojan say to the Bruin?\n',
+                           '\tWhat do you get when you cross a Data Scientist with an AI Engineer??\n',
+                           '\tWhere did the pig go to buy some food?\n'])
 
 # Let's see what was generated!
 for punchline in punchlines:

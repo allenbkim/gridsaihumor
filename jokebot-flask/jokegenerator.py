8113fe695f41db3014af1a9cb1135bf5d5ed45bb
Allen Kim <allenbkim@gmail.com>
2019-08-16 23:07:47 -0700

    Sync changes from last update to main generator and make compatibility changes for AWS deployment

--- jokebot-flask/jokegenerator.py
+++ jokebot-flask/jokegenerator.py
@@ -5,11 +5,12 @@ import numpy as np
 import pickle
 import os
 
+
 # Constants used in execution
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))   # refers to application_top
 APP_STATIC = os.path.join(APP_ROOT, 'static')
-ENCODER_MODEL_FILE = APP_ROOT + '/gridsai-qahumor-encoder-model.h5'
-DECODER_MODEL_FILE = APP_ROOT + '/gridsai-qahumor-decoder-model.h5'
+ENCODER_MODEL_FILE = APP_ROOT + '/gridsai-qahumor-encoder-model_modified_240_.h5'
+DECODER_MODEL_FILE = APP_ROOT + '/gridsai-qahumor-decoder-model_modified_240_.h5'
 ENCODER_PICKLE_FILE = APP_ROOT + '/gridsai-qahumor-encoder-pickle.pckl'
 DECODER_PICKLE_FILE = APP_ROOT + '/gridsai-qahumor-decoder-pickle.pckl'
 CHAR_TOKENS = ['\n', '\t', ' ', '.', ',', '!', '?', ':', ';', '$', '#', '@', '%', '^', '&', '*', '(', ')', '-', '_',
@@ -23,48 +24,74 @@ CHAR_TOKENS = ['\n', '\t', ' ', '.', ',', '!', '?', ':', ';', '$', '#', '@', '%'
 encoder_model = None
 decoder_model = None
 graph = None
+max_encoder_seq_length = None
+max_decoder_seq_length = None
+num_tokens = None
+token_index = None
+reverse_char_index = None
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
 def setup():
-    global encoder_model
-    encoder_model = load_model(ENCODER_MODEL_FILE)
-    global decoder_model
-    decoder_model = load_model(DECODER_MODEL_FILE)
-    global graph
-    graph = tf.get_default_graph()
+  global encoder_model
+  encoder_model = load_model(ENCODER_MODEL_FILE, compile=False)
+  global decoder_model
+  decoder_model = load_model(DECODER_MODEL_FILE, compile=False)
+  global graph
+  graph = tf.get_default_graph()
+
+  global max_encoder_seq_length
+  encoder_pickle_file = open(ENCODER_PICKLE_FILE, 'rb')
+  max_encoder_seq_length = pickle.load(encoder_pickle_file)
+  encoder_pickle_file.close()
+  global max_decoder_seq_length
+  #decoder_pickle_file = open(DECODER_PICKLE_FILE, 'rb')
+  #max_decoder_seq_length = pickle.load(decoder_pickle_file)
+  max_decoder_seq_length = 50
+  #decoder_pickle_file.close()
+
+  # Reverse-lookup token index to decode sequences back to something readable.
+  global num_tokens
+  global token_index
+  global reverse_char_index
+  characters = sorted(list(CHAR_TOKENS))
+  num_tokens = len(characters)
+  token_index = dict([(char, i) for i, char in enumerate(characters)])
+  reverse_char_index = dict((i, char) for char, i in token_index.items())
 
 
 setup()
 
 
-@app.route('/', methods=['POST', 'GET'])
+@application.route('/', methods=['POST', 'GET'])
 def generate_joke():
     if request.method == 'GET':
         return render_template('jokes.html')
     elif request.method == 'POST':
-        joke = '\t' + request.form['userJoke'] + '\n'
-        answer = forward_pass([joke])[0][1]
-        return render_template('jokes.html', userJoke=request.form['userJoke'], punchline=answer)
+        joke = [('\t' + request.form['userJoke'], request.form['userJoke'])]
+        answer = forward_pass(joke)
+        return render_template('jokes.html', userJoke=request.form['userJoke'], punchline=answer[0][1][1])
 
 
-def decode_sequence(input_seq, num_decoder_tokens, target_token_index, reverse_target_char_index, max_decoder_seq_length):
+def decode_sequence(input_seq, seed_char, num_decoder_tokens, target_token_index, reverse_target_char_index, max_decoder_seq_length):
+    # Encode the input as state vectors.
     with graph.as_default():
-        states_value = encoder_model.predict(input_seq)
+      states_value = encoder_model.predict(input_seq)
 
     # Generate empty target sequence of length 1.
     target_seq = np.zeros((1, 1, num_decoder_tokens))
     # Populate the first character of target sequence with the start character.
-    target_seq[0, 0, target_token_index['\t']] = 1.
+    target_seq[0, 0, target_token_index[seed_char]] = 1.
 
     # Sampling loop for a batch of sequences
     # (to simplify, here we assume a batch of size 1).
     stop_condition = False
-    decoded_sentence = ''
+    decoded_sentence = seed_char
     while not stop_condition:
-        with graph.as_default():
-            output_tokens, h, c = decoder_model.predict([target_seq] + states_value)
+      with graph.as_default():
+        output_tokens, h, c = decoder_model.predict(
+            [target_seq] + states_value)
 
         # Sample a token
         sampled_token_index = np.argmax(output_tokens[0, -1, :])
@@ -74,8 +101,11 @@ def decode_sequence(input_seq, num_decoder_tokens, target_token_index, reverse_t
         # Exit condition: either hit max length
         # or find stop character.
         if (sampled_char == '\n' or
-           len(decoded_sentence) > max_decoder_seq_length):
-            stop_condition = True
+            sampled_char == '.' or
+            sampled_char == '!' or
+            sampled_char == '?' or
+	          len(decoded_sentence) > max_decoder_seq_length):
+          stop_condition = True
 
         # Update the target sequence (of length 1).
         target_seq = np.zeros((1, 1, num_decoder_tokens))
@@ -86,7 +116,6 @@ def decode_sequence(input_seq, num_decoder_tokens, target_token_index, reverse_t
 
     return decoded_sentence
 
-
 def forward_pass(jokes_needing_punchlines):
     """
     Generates
@@ -94,32 +123,19 @@ def forward_pass(jokes_needing_punchlines):
     :return: List of 2-tuples containing joke setup/generated punchline pairs.
     """
     decoded_sentences = []
-
-    encoder_pickle_file = open(ENCODER_PICKLE_FILE, 'rb')
-    max_encoder_seq_length = pickle.load(encoder_pickle_file)
-    encoder_pickle_file.close()
-    decoder_pickle_file = open(DECODER_PICKLE_FILE, 'rb')
-    max_decoder_seq_length = pickle.load(decoder_pickle_file)
-    decoder_pickle_file.close()
-
-    # Reverse-lookup token index to decode sequences back to something readable.
-    characters = sorted(list(CHAR_TOKENS))
-    num_tokens = len(characters)
-    token_index = dict([(char, i) for i, char in enumerate(characters)])
-    reverse_char_index = dict((i, char) for char, i in token_index.items())
-
     encoder_input_data = np.zeros(
         (len(jokes_needing_punchlines), max_encoder_seq_length, num_tokens),
         dtype='float32')
-
     for i, input_text in enumerate(jokes_needing_punchlines):
-        for t, char in enumerate(input_text):
+        for t, char in enumerate(input_text[0]):
             encoder_input_data[i, t, token_index[char]] = 1.
-
     for seq_index in range(len(jokes_needing_punchlines)):
         # Take one sequence for trying out decoding.
         input_seq = encoder_input_data[seq_index: seq_index + 1]
-        decoded_sentence = decode_sequence(input_seq, num_tokens, token_index, reverse_char_index, max_decoder_seq_length)
-        decoded_sentences.append((jokes_needing_punchlines[seq_index], decoded_sentence))
+        decoded_sentence = decode_sequence(input_seq, jokes_needing_punchlines[seq_index][1][0], num_tokens, token_index, reverse_char_index, max_decoder_seq_length)
+        decoded_sentences.append((jokes_needing_punchlines[seq_index][1][0], (jokes_needing_punchlines[seq_index][0], decoded_sentence[1:].strip())))
 
     return decoded_sentences
+
+if __name__ == '__main__':
+  application.run()
